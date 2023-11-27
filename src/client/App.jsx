7d8102517d5e2c41914794fc897f2facdb90ebeb
Linus <contact@linusskold.com>
2023-11-28 00:58:38 +0100

    github actions deploy test

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,6 +7,12 @@ export default function App() {
 	return (
 		<div>
 			<h1>Linus Sköld</h1>
+			<h2>Software Developer</h2>
+			<li>Github</li>
+			<li>LinkedIn</li>
+			<li>CV</li>
+			<li></li>
+			
 			<section>
 				<img id="profile" src='./static/self.png' />
 
